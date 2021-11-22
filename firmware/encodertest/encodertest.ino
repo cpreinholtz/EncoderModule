5ccb2d0cd4275f7685e96b18b06a15b7b664e9d6
cpreinholtz <cpr978@gmail.com>
2021-11-22 10:42:18 -0800

    add object oriented implimentation of encoder motion tracking with the hope that is is easly expandable to multiple knobs in the future

--- firmware/encodertest/encodertest.ino
+++ firmware/encodertest/encodertest.ino
@@ -2,53 +2,14 @@
 // http://makeatronics.blogspot.com/2013/02/efficiently-reading-quadrature-with.html
 
 
-//set this to 0 for interupt usage 1 for polling
-#define USE_POLLING 0
-
-volatile long enc_count = 0; 
+#include "grey_counter.h"
 
 
 
-void encoder_isr() {
-    static int8_t lookup_table[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
-    static uint8_t enc_val = 0;
-    
-    enc_val = enc_val << 2;
-    
-    //for this to work, the encoder needs to be on pins 3 and 4
-    //PIND is an easy way to read input pins on the arduino, returning the value of pins 0 - 7.  
-    //we mask and shiftright to get only pins 3 and 4
-    enc_val = enc_val | ((PIND & 0b11000) >> 3);
- 
-    enc_count = enc_count + lookup_table[enc_val & 0b1111];
-    Serial.print("interupt result: ");
-    Serial.println(enc_count);
 
-    
-}
-
-
-
-void encoder_poll(uint8_t thisVal) {
-    static int8_t lookup_table[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
-    //this really acts as a 
-    static uint8_t enc_val = 0;
-
-    //check for change ( this isnt even really necisarry honestly
-    
-    //if (thisVal != enc_val & 0b11){
-    
-        
-        //shift the last value to the left, add on this val
-        enc_val = (enc_val << 2) | (thisVal & 0b011);
-        
-        //lookup wheather we want +1 -1 +0 based on the last and previous readings
-        enc_count = enc_count + lookup_table[enc_val & 0b00001111];
-        Serial.print("polling result: ");
-        Serial.println(enc_count);
-
-    //}
-}
+const int gA= 3;
+const int gB= 4;
+GreyCounter gc;
 
 
 
@@ -56,17 +17,17 @@ void encoder_poll(uint8_t thisVal) {
 void setup() {
     // all your normal setup code
 
-    pinMode(3,INPUT_PULLUP);
-    pinMode(4,INPUT_PULLUP);
+    pinMode(gA,INPUT_PULLUP);
+    pinMode(gB,INPUT_PULLUP);
     
-#if not USE_POLLING
-    attachInterrupt(3,encoder_isr,CHANGE);
-    attachInterrupt(4,encoder_isr,CHANGE);
-#endif
 
     Serial.begin(9600);
     Serial.println("here");
-    
+
+
+    //gc.initCounter( (digitalRead(gA)<<1)  | digitalRead(gB) );
+    gc.initCounter( digitalRead(gA), digitalRead(gB) );
+
 }
 
 
@@ -75,20 +36,14 @@ void setup() {
 
 void loop()
 {
+
     
-#if USE_POLLING
+    //GreyState gs = gc.updateCounter(digitalRead(gA), digitalRead(gB));    
+    GreyState gs = gc.updateCounter( (digitalRead(gA)<<1)  | digitalRead(gB) );
 
-    //for this to work, the encoder needs to be on pins 3 and 4
-    //PIND is an easy way to read input pins on the arduino, returning the value of pins 0 - 7.  
-    //we mask and shiftright to get only pins 3 and 4
-    uint8_t thisVal = ((PIND & 0b11000) >> 3); 
-    encoder_poll( thisVal);
-
-    delay(1);
-#endif
-
-
-
-
+    
+    if (gs.sChanged){
+        Serial.println(gs.sCount);
+    }    
     
 }
---
+++ firmware/encodertest/grey_counter.h
@@ -0,0 +1,109 @@
+#include <inttypes.h>
+
+
+struct GreyState
+{
+    bool sChanged;
+    long sCount;
+};
+
+
+
+
+//This class keeps track of the motion of a rotary encoder
+//when updating, the class returns the state structure that tells the current tick count and weather or not anything has changed since the last update
+//the update function is meant to be called frequently (>100Hz ish) whenever the encoder pins are polled
+
+//encoder counter updates based on this resource:
+//http://makeatronics.blogspot.com/2013/02/efficiently-reading-quadrature-with.html
+
+class GreyCounter
+{
+private:
+    GreyState mState;
+    uint8_t mShiftReg;
+
+    //this is the basis of counter updates,  it uses the laststate<<2 | currentstate
+    //See Doc/quadratureTable.png for full table breakdown
+    const int8_t kLookupTable[16] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
+
+
+
+
+public:
+
+    //**************************************************************
+    //Update current state
+    //**************************************************************
+    //thisVal is a 2 bit binary : 000000ab  where b is the least significant bit
+    GreyState updateCounter(uint8_t thisVal){
+        //mask low 2 bits
+        thisVal = thisVal & 0b11;
+        
+        //shift into shiftreg
+        mShiftReg = (mShiftReg << 2) | (thisVal);
+
+        //use lookup to get dif
+        int8_t dif = kLookupTable[mShiftReg & 0b00001111];  
+        mState.sChanged = dif !=0;        
+        mState.sCount = mState.sCount + dif;
+        return mState;
+    }
+
+    
+
+    //A and b given seperatly in this overloaded implimentation, concatenate A and B and feed to the original update function
+    GreyState updateCounter(uint8_t a, uint8_t b){
+        //thisVal is a 2 bit binary : 000000ab  where b is the least significant bit
+        uint8_t thisVal = ((a&0b1) << 1) |  (b & 0b1);                
+        return GreyCounter::updateCounter(thisVal);
+    }
+
+
+
+    //**************************************************************
+    // Getters
+    //**************************************************************
+    bool getChanged(){
+        return mState.sChanged;        
+    }
+    
+    bool getCount(){
+        return mState.sCount;        
+    }
+    
+
+
+    //**************************************************************
+    // Consturctors
+    //**************************************************************
+    GreyCounter(){ 
+        mShiftReg = 0; //This alone will not ensure an initial count of 0 because the shiftreg is not representative of the actual encoder state
+        mState.sChanged = false;    
+        mState.sCount = 0;   
+    }
+
+
+    //Used to ensure the starting count is truly 0 by setting the shiftreg properly
+    //A and b given seperatly in this overloaded implimentation, concatenate A and B and feed to the original init function
+    void initCounter(uint8_t a, uint8_t b){
+        //thisVal is a 2 bit binary : 000000ab  where b is the least significant bit
+        uint8_t thisVal = ((a&0b1) << 1) |  (b & 0b1);       
+        initCounter(thisVal);        
+    }
+
+    //Used to ensure the starting count is truly 0 by setting the shiftreg properly
+    //thisVal is a 2 bit binary : 000000ab  where b is the least significant bit
+    void initCounter(uint8_t thisVal){
+        //mask low 2 bits
+        thisVal = thisVal & 0b11;        
+        //shift into shiftreg
+        mShiftReg = (mShiftReg << 2) | (thisVal);
+        mState.sChanged = false;    
+        mState.sCount = 0;   
+    }
+
+    
+
+
+};// end class definition
