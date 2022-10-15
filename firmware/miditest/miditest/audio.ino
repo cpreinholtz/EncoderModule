754370a31dd5b7f5ec611f6ad1eb0ee27661a7bf
cpreinholtz <cpr978@gmail.com>
2022-10-15 13:14:51 -0700

    Add chord_maker from my midi library at https://github.com/cpreinholtz/midi.git

--- firmware/miditest/miditest/audio.ino
+++ firmware/miditest/miditest/audio.ino
@@ -1,4 +1,4 @@
-
+#include <chord_maker.h>
 
 AudioOutputI2S      audioOutput;        // audio shield: headphones & line-out
 AudioControlSGTL5000 audioShield;
@@ -28,6 +28,14 @@ AudioMixer4              gMixerFxR;       //xy=1287.666748046875,777.33337402343
 AudioConnection patchOut0 (gMixerFxL, 0, audioOutput , 0);
 AudioConnection patchOut1 (gMixerFxR, 0, audioOutput , 1);
 
+// -----------------------------------------------------------------------------
+bool chordMode;
+bool dummyMode;
+bool splitMode;
+byte splitKey;
+extern const int LED_PIN;
+ChordMaker chordMaker;
+
 
 
 void initAudio(){
@@ -46,6 +54,10 @@ void initAudio(){
     }
 
     gVoices.initAudio();
+    chordMode = false;
+    dummyMode = false;
+    splitMode = false;
+    splitKey = 0;
 }
 
 
@@ -78,26 +90,105 @@ void updateNotes(){
 }
   
 
-// -----------------------------------------------------------------------------
+
+
+
+void toggleSettings(byte note){
+    switch(note%12){
+        case 0: 
+            chordMode = !chordMode;
+            digitalWrite(LED_PIN,chordMode);
+            break;
+        case 2:
+            dummyMode = !dummyMode;
+            digitalWrite(LED_PIN,dummyMode);
+            break;
+        case 4:
+            splitMode = !splitMode;
+            digitalWrite(LED_PIN,splitMode);
+            break;
+        default:
+            Serial.println("bad toggle signal");
+            break;            
+    }
+}
+
+
+void defaultNoteOn(byte note, byte vel){
+    chordMaker.setRoot(note);
+    //check for dummys
+    if ( dummyMode ==true and chordMaker.getValid() ==false) return; //do nothing!!! prevent dummy from playing bad note
+
+    //else valid chord, or dummy mode disabled
+    gNotes.noteOn(note, vel);
+    
+    //check if we want to make this a chord
+    if (chordMode==true and chordMaker.getValid() ==true and splitMode==false or (splitMode==true and note<splitKey) ){
+        gNotes.noteOn(chordMaker.getTriadMiddle(), vel);
+        gNotes.noteOn(chordMaker.getTriadLast(), vel);
+    }
+    updateNotes();//this contains voices.set
+}
+
+
+void defaultNoteOff(byte note){
+    chordMaker.setRoot(note);
+    //check for dummys
+    //if ( dummyMode ==true and chordMaker.getValid() ==false) return; //do nothing!!! prevent dummy from playing bad note
+
+    //else valid chord, or dummy mode disabled
+    gNotes.noteOff(note);
+    gVoices.unset(note);
+    //check if we wanted to make this a chord
+    if (chordMode==true and chordMaker.getValid() ==true and splitMode==false or (splitMode==true and note<splitKey) ){
+        gNotes.noteOff(chordMaker.getTriadMiddle()); 
+        gVoices.unset(chordMaker.getTriadMiddle());
+        gNotes.noteOff(chordMaker.getTriadLast());
+        gVoices.unset(chordMaker.getTriadLast());
+    }
+}
 
 void handleNoteOn(byte ch, byte note, byte vel){
     Serial.println("on");
     if (vel <= 0){
         Serial.println("Velocity 0!!!!!!!!!!!!!!");
-        gNotes.noteOff(note);
-        gVoices.unset(note);
+        handleNoteOff(ch, note, vel);
     } else {
-        gNotes.noteOn(note, vel);
-        updateNotes();//this contains voices.set
+        switch(ch) {
+            case 15://used for toggling settings
+                toggleSettings(note);
+                break;
+            case 14://for seting key
+                chordMaker.setKey(note);
+                break;
+            case 13://for setting split
+                splitKey = note;
+                break;
+            default:
+                defaultNoteOn(note, vel);
+                break;
+        }            
     }
     
 }
 
 void handleNoteOff(byte ch, byte note, byte vel){
     //Serial.println("off");
-    gNotes.noteOff(note);
-    gVoices.unset(note);
-    //updateNotes();
+    switch(ch) {
+        case 15://used for toggling settings
+            //toggleSettings(note);
+            break;
+        case 14://for seting key
+            //chordMaker.setKey(note);
+            break;
+        case 13://for setting split
+            //splitKey = note;
+            break;
+        default:
+            defaultNoteOff(note);
+            break;
+                    
+    }
 }
 
 
